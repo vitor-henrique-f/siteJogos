e32ec224a683b2f0ecdd80ae1e3ecab787e270ab
vitor-henrique-f <vitorcamposfaz@gmail.com>
2026-05-13 22:42:58 -0300

    Final touchs

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
-  const video = document.getElementById('blackHole');
-  video.playbackRate = 0.7;
+const video = document.getElementById('blackHole');
+video.playbackRate = 0.8;
+
+const slides = document.querySelector(".slides");
+const slide = document.querySelectorAll(".slide");
+
+const leftArrow = document.getElementById("lArrow");
+const rightArrow = document.getElementById("rArrow");
+
+let index = 0;
+
+function updateSlide(){
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+rightArrow.addEventListener("click", () => {
+
+    index++;
+
+    if(index >= slide.length){
+        index = 0;
+    }
+
+    updateSlide();
+});
+
+leftArrow.addEventListener("click", () => {
+
+    index--;
+
+    if(index < 0){
+        index = slide.length - 1;
+    }
+
+    updateSlide();
+});
